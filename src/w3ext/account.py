674912e827d26eb0c1b7e7b77d7bed50c98910d9
cheewba <chewba34@gmail.com>
2023-09-18 14:12:10 +0200

    Change the way account can be used to interact with Chain

--- src/w3ext/account.py
+++ src/w3ext/account.py
@@ -1,7 +1,5 @@
 # pylint: disable=no-name-in-module
-import os
 from contextlib import contextmanager
-from functools import wraps
 from typing import TypeVar, Any, TYPE_CHECKING
 
 from eth_account.signers.local import LocalAccount
@@ -16,52 +14,63 @@ __all__ = ["Account", ]
 
 Self = TypeVar("Self")
 
-ABI_PATH = os.path.join(os.path.dirname(__file__), 'abi')
-
-
-def _onchain_required(fn):
-    @wraps(fn)
-    def inner(self: "Account", *args, **kwargs):
-        assert self._chain is not None, f"Chain context is required for {fn.__name__}"
-        return fn(self, *args, **kwargs)
-    return inner
-
 
 class Account:
-    __acc: LocalAccount = None
-    _chain: "Chain" = None
+    """ Wrapper around Web3 Account, not bound to any ``Chain`` instance. """
+    _acc: LocalAccount = None
 
     @classmethod
     def from_key(cls, key: str) -> 'Account':
-        instance = Account()
-        instance.__acc = Web3Account.from_key(key)
+        instance = cls()
+        instance._acc = Web3Account.from_key(key)
         return instance
 
-    @_onchain_required
-    async def get_balance(self, token: 'Token' = None) -> 'CurrencyAmount':
-        fn = token.get_balance if isinstance(token, Token) else self._chain.get_balance
-        return await fn(self.address)
+    def use_chain(self, chain: "Chain") -> "ChainAccount":
+        return ChainAccount(self, chain)
 
     @contextmanager
-    def onchain(self, chain: "Chain") -> Self:
-        added_middleware = False
+    def onchain(self, *chains: "Chain") -> Self:
+        chains_processed = []
         try:
-            old, self._chain = self._chain, chain
-            if not chain.middleware_onion.get(self.address):
-                chain.middleware_onion.add(
-                    construct_async_sign_and_send_raw_middleware(self.__acc),
-                    self.address,
-                )
-                added_middleware = True
-            yield self
+            for chain in chains:
+                if not chain.middleware_onion.get(self.address):
+                    chain.middleware_onion.add(
+                        construct_async_sign_and_send_raw_middleware(self._acc),
+                        self.address,
+                    )
+                    chains_processed.append(chain)
+            bound = [self.use_chain(chain) for chain in chains]
+            yield bound[0] if len(bound) == 1 else bound
         finally:
-            self._chain = old
-            if added_middleware:
+            for chain in chains_processed:
                 chain.middleware_onion.remove(self.address)
 
     def __getattr__(self, name) -> Any:
         # let use token as a contract with predefined ABI and web3 instance
-        return getattr(self.__acc, name)
+        return getattr(self._acc, name)
 
     def __str__(self) -> str:
         return self.address
+
+
+class ChainAccount:
+    """ Account bound to the ``Chain`` instance. """
+    _account: "Account"
+    _chain: "Chain"
+
+    def __init__(self, account: "Account", chain: "Chain") -> None:
+        self._account = account
+        self._chain = chain
+
+    def chain(self) -> "Chain":
+        return self._chain
+
+    async def get_balance(self, token: 'Token' = None) -> 'CurrencyAmount':
+        fn = token.get_balance if isinstance(token, Token) else self._chain.get_balance
+        return await fn(self.address)
+
+    def __getattr__(self, name) -> Any:
+        return getattr(self._account, name)
+
+    def __str__(self) -> str:
+        return f"{self._chain}({self._account})"
